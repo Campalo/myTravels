b4438f6dff61c37fc4fc1e41f20e8dbe03c167f5
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-11 22:11:23 +0200

    add travel-db.js

---
+++ src/travel-db.js
@@ -0,0 +1,42 @@
+// The list of travelItems
+
+const myTravels = [
+  {
+      id: 0,
+      photo : "https://www.carigami.fr/magazine/wp-content/uploads/2014/09/sydney-australie.jpg",
+      destination : " Syndey",
+      country : "Australia",
+      distance : "16 084 km " ,  
+  },
+  {
+      id: 1,
+      photo : "https://upload.wikimedia.org/wikipedia/commons/f/f2/Merlion_and_the_Singapore_Skyline.JPG",
+      destination : " Singapore",
+      country : "Singapore",
+      distance : "9 900 km ",
+  },
+  {
+      id: 2,
+      photo : "https://upload.wikimedia.org/wikipedia/commons/8/83/Harbour_Bridge_from_Chelsea.JPG",
+      destination : " Auckland",
+      country : "New Zeland",
+      distance : "17 500 km ",
+  },
+  {
+      id : 3,
+      photo : "https://upload.wikimedia.org/wikipedia/commons/b/b6/GrooteKerk_001.jpg",
+      destination : " Pretoria",
+      country : "South Africa",
+      distance : "8 800 km ",
+  },
+  {
+      id : 4,
+      photo : "https://upload.wikimedia.org/wikipedia/commons/f/f9/CentrevilleMontreal%C3%A9t%C3%A9.jpg",
+      destination : " Montreal",
+      country : "Canada",
+      distance : "6 000 km ",
+  }
+];
+
+
+export default myTravels;
