728f119031cbd09fd219568dc44023cc6d672c3a
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-11 18:24:23 +0200

    update Travels.js + add design to App.css

--- src/Travels.js
+++ src/Travels.js
@@ -17,27 +17,28 @@ const mytravels = [
         distance : "9 900 km ",
     },
     {
-        photo : "https://upload.wikimedia.org/wikipedia/commons/f/f2/Merlion_and_the_Singapore_Skyline.JPG",
-        destination : " Singapore",
-        country : "Singapore",
-        distance : "9 900 km ",
+        photo : "https://upload.wikimedia.org/wikipedia/commons/8/83/Harbour_Bridge_from_Chelsea.JPG",
+        destination : " Auckland",
+        country : "New Zeland",
+        distance : "17 500 km ",
     },
     {
-        photo : "https://upload.wikimedia.org/wikipedia/commons/f/f2/Merlion_and_the_Singapore_Skyline.JPG",
-        destination : " Singapore",
-        country : "Singapore",
-        distance : "9 900 km ",
+        photo : "https://upload.wikimedia.org/wikipedia/commons/b/b6/GrooteKerk_001.jpg",
+        destination : " Pretoria",
+        country : "South Africa",
+        distance : "8 800 km ",
     },
     {
-        photo : "https://upload.wikimedia.org/wikipedia/commons/f/f2/Merlion_and_the_Singapore_Skyline.JPG",
-        destination : " Singapore",
-        country : "Singapore",
-        distance : "9 900 km ",
+        photo : "https://upload.wikimedia.org/wikipedia/commons/f/f9/CentrevilleMontreal%C3%A9t%C3%A9.jpg",
+        destination : " Montreal",
+        country : "Canada",
+        distance : "6 000 km ",
     }
 ];
 
 const Travels = () => (
     <div>
+        {/* Using keys with index */}
         {mytravels.map((travelItem, index) => (
             <Travel key={"dream" + index}
                 photo={travelItem.photo}
