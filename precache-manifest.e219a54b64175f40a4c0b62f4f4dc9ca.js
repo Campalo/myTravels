7b2b6292064257233a582c06ec9dcd17a6137d33
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-11 22:22:26 +0200

    Updates

---
+++ precache-manifest.e219a54b64175f40a4c0b62f4f4dc9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93ccf155fab5a2675e5a",
+    "url": "/myTravels/static/css/main.6fc64bc3.chunk.css"
+  },
+  {
+    "revision": "93ccf155fab5a2675e5a",
+    "url": "/myTravels/static/js/main.77f3f7e5.chunk.js"
+  },
+  {
+    "revision": "2773745ae0c8119ba14c",
+    "url": "/myTravels/static/js/runtime~main.04289843.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/myTravels/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myTravels/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d847b6b1201cfa823e0691c67dd788f2",
+    "url": "/myTravels/index.html"
+  }
+];
